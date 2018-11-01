844e67f7614ec403c33a2b8cc8e687733526b40f
Jonathan Bordeleau <jonathan.bordeleau@polymtl.ca>
2018-11-01 10:07:07 -0400

    cube

--- TP4Code/Cube.cpp
+++ TP4Code/Cube.cpp
@@ -17,35 +17,37 @@ Cube::Cube(const Point3D& pt, float ln, float lg, float ht)
 	// A Completer...
 }
 
-Cube::~Cube(){
+Cube::~Cube() {
 }
 
 Cube * Cube::clone() const
 {
 	// A Completer...
-	return nullptr;
+	Cube* cubeClone;
+	return cubeClone = new Cube(Point3D(m_center), m_dimensions[0], m_dimensions[1], m_dimensions[2]);
 }
 
 size_t Cube::getNbParameters() const {
-
 	// A Completer...
-	return 0;
+	return 3;
 }
 
-PrimitiveParams Cube::getParameters() const 
+PrimitiveParams Cube::getParameters() const
 {
 	// A Completer...
-	return PrimitiveParams();
+	PrimitiveParams params{ m_dimensions[0], m_dimensions[1], m_dimensions[2] };
+	return params;
 }
 
-void Cube::setParameter(size_t pIndex, float pValue){
+void Cube::setParameter(size_t pIndex, float pValue) {
 	if (pIndex < 0 || pIndex > 2)
 		throw std::range_error("Invalid parameter index for cube. Must be between 0 and 2");
 
-	if(pValue<0.0)
+	if (pValue<0.0)
 		throw std::range_error("Invalid dimension value for cube. Must be larger than 0");
 
 	// A Completer...
+	m_dimensions[pIndex] = pValue;
 }
 
 std::ostream & Cube::toStream(std::ostream & o) const
